df60d679213a72741297eff2e5c32b9e9a8cb572
Edoardo Tarulli <S1094923@studenti.univpm.it>
2021-05-06 18:30:54 +0200

    controller_tabella_orari

---
+++ TabellaOrari/controller/controller_tabella_orari.py
@@ -0,0 +1,15 @@
+from TabellaOrari.model.tabella_orari import tabella_orari
+
+class Controller_tabella_orari:
+    def __init__(self, numero_tabella):
+        self.model = tabella_orari(numero_tabella)
+
+    def aggiungi_a_giorno(self, giorno, dipendente):
+        self.model.aggiungi_a_giorno(giorno, dipendente)
+
+    def rimuovi_da_giorno(self, giorno, dipendente):
+        self.model.rimuovi_da_giorno(giorno, dipendente)
+
+    def get_lista_tabella_orari(self):
+        return self.model.get_lista_tabella_orari()
+
