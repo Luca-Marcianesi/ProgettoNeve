4412a65ff13290d726e4645d944d00d993075bd6
Diego Mignani <S1093616@studenti.univpm.com>
2021-05-07 17:34:08 +0200

    correzzione alcuni errori

--- ListaPiste/model/lista_piste.py
+++ ListaPiste/model/lista_piste.py
@@ -24,7 +24,7 @@ class lista_piste:
         self.lista_piste.append(pista)
 
     def leggi_dati(self):
-        with open("Pista/data/lista_piste.json") as file:
+        with open("ListaPiste/data/lista_piste.json") as file:
             lista_piste_inizio = json.load(file)
         for pista_da_caricare in lista_piste_inizio:
             self.aggiungi_pista(pista(pista_da_caricare["nome"],pista_da_caricare["colore"],pista_da_caricare["stato"]))
