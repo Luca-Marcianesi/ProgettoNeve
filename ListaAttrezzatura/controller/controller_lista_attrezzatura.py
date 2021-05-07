84eaf381d296f15399e04bbaa1965ba0f1838703
Edoardo Tarulli <S1094923@studenti.univpm.it>
2021-05-07 15:47:07 +0200

    controller_tabella_orari

---
+++ ListaAttrezzatura/controller/controller_lista_attrezzatura.py
@@ -0,0 +1,17 @@
+from ListaAttrezzatura.model.lista_attrezzatura import listaAttrezzatura
+
+class controller_lista_attrezzatura:
+    def __init__(self):
+        self.model = listaAttrezzatura()
+
+    def aggiungi_attrezzatura(self, attrezzatura):
+        self.model.aggiungi_attrezzatura(attrezzatura)
+
+    def rimuovi_attrezzatura(self, indice):
+        self.model.rimuovi_attrezzatura(indice)
+
+    def set_stato(self, stato):
+        self.model.set_stato(stato)
+
+    def prenota_attrezzatura(self, tipo):
+        self.model.prenota_attrezzatura(tipo)
