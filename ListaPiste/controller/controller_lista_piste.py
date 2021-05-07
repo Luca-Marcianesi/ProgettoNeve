8c50bb137300c8e7d1f642187410deb6ab9a9baa
Edoardo Tarulli <S1094923@studenti.univpm.it>
2021-05-07 16:15:37 +0200

    controller_lista_piste

--- ListaPiste/controller/controller_lista_piste.py
+++ ListaPiste/controller/controller_lista_piste.py
@@ -2,3 +2,21 @@ from ListaPiste.model.lista_piste import lista_piste
 class controller_lista_piste():
     def __init__(self):
         self.model = lista_piste()
+
+    def cerca_piste(self, nome):
+        self.model.lista_piste(nome)
+
+    def cerca_pista_x_numero(self, posizione):
+        self.model.cerca_pista_x_numero(posizione)
+
+    def aggiungi_pista(self, pista):
+        self.model.aggiungi_pista(pista)
+
+    def leggi_dati(self):
+        self.model.leggi_dati()
+
+    def modifica_pista(self, posizione, stato):
+        self.model.modifica_pista(posizione, stato)
+
+    def visualizza_pista(self, numero):
+        self.model.visualizza_pista(numero)
