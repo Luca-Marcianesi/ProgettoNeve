1cfaef72396ea6cc0c103f7ae0e22956f9466e1e
luca1 <s1093754@studenti.univpm.it>
2021-05-10 12:29:42 +0200

    attrezzatura.py

--- Attrezzatura/model/attrezzatura.py
+++ Attrezzatura/model/attrezzatura.py
@@ -5,11 +5,17 @@ class attrezzatura:
         self.codice = codice
         self.dimensioni = dimensioni
 
-    def get_tipo(self):
-        return self.tipo
+    def get_codice(self):
+        return self.codice
+
+    def get_nome(self):
+        return self.nome
+
+    def get_dimensioni(self):
+        return self.dimensioni
+
+    def get_stato(self):
+        return self.stato
 
     def set_stato(self, stato):
         self.stato = stato
-
-    def set_nome(self, nome):
-        self.nome = nome
