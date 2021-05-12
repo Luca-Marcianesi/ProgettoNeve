307996c938e15f16793a49bc924c26097222cb78
luca1 <s1093754@studenti.univpm.it>
2021-05-12 13:09:59 +0200

    sessione

--- Pista/vista/VistaPista.py
+++ Pista/vista/VistaPista.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QStandardItemModel, QStandardItem, QFont, QBrush, QPalette, QImage
+from PyQt5.QtGui import QStandardItemModel, QStandardItem, QFont, QBrush, QPalette, QImage, QColor
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QSpacerItem, QSizePolicy, QListView, QPushButton, \
     QDesktopWidget
 from Pista.controller.controller_pista import controller_pista
@@ -12,27 +12,39 @@ class vista_pista(QWidget):
 
         # Attributi
         lista = lista_piste()
-        self.controller = controller_pista(lista.cerca_pista_x_numero(4))
+        self.controller = controller_pista(lista.cerca_pista_x_numero(15))
         self.layout_verticale = QVBoxLayout()
         self.layout_orizzontale = QHBoxLayout()
+        self.layout_verticale2 = QVBoxLayout()
 
 
         # Sfondo
         self.show_background("PISTA")
 
         #Descrizione Pista
-        label = QLabel("Nome => {}\n".format(self.controller.get_nome_str()) + "\n"
-                        "Difficoltà => {}\n".format(self.controller.get_difficolta()) + "\n"
-                        "Stato => {}\n".format(self.controller.get_stato()) + "\n")
+        label = QLabel("\nNOME => {}\n".format(self.controller.get_nome_str()) +
+                        "DIFFICOLTÀ => {}\n".format(self.controller.get_difficolta()) +
+                        "STATO => {}\n".format(self.controller.get_stato()) )
         label.setFont(QFont('Times New Roman', 30))
+        label.setStyleSheet("background-image:url(Pista/data/download.jpg)")
         label.setAlignment(Qt.AlignCenter)
-        self.layout_verticale.addWidget(label)
+        self.layout_orizzontale.addSpacerItem(QSpacerItem(500, 0, QSizePolicy.Fixed, QSizePolicy.Fixed))
+        self.layout_verticale2.addWidget(label)
+
+        self.layout_verticale2.addSpacerItem(QSpacerItem(0, 150, QSizePolicy.Fixed, QSizePolicy.Fixed))
+
+        # Indietro allineati
+        self.show_pulsantiera()
+
+        self.layout_orizzontale.addLayout(self.layout_verticale2)
+
+        self.layout_orizzontale.addSpacerItem(QSpacerItem(500, 0, QSizePolicy.Fixed, QSizePolicy.Fixed))
+
+
+
 
         self.layout_verticale.addSpacerItem(QSpacerItem(0, 50, QSizePolicy.Fixed, QSizePolicy.Fixed))
         self.layout_verticale.addLayout(self.layout_orizzontale)
-
-        #Indietro allineati
-        self.show_pulsantiera()
 
         self.layout_verticale.addSpacerItem(QSpacerItem(0, 275, QSizePolicy.Fixed, QSizePolicy.Fixed))
 
@@ -54,7 +66,7 @@ class vista_pista(QWidget):
         # Sfondo
         self.setFixedWidth(QDesktopWidget().width())
         self.setFixedHeight(QDesktopWidget().height())
-        back_img = QImage("ListaAccount/data/im.jpg")
+        back_img = QImage("Pista/data/Sarnano_Sassotetto.jpg")
         img = back_img.scaled(self.width(), self.height())
         palette = QPalette()
         palette.setBrush(10, QBrush(img))
@@ -72,9 +84,12 @@ class vista_pista(QWidget):
     def show_pulsantiera(self):
 
         # Punsante indietro
+        layout_pulsanti = QHBoxLayout()
         pulsante_indietro = QPushButton("INDIETRO")
         pulsante_indietro.setFont(QFont('Times New Roman', 18))
         pulsante_indietro.setFixedSize(250, 100)
         pulsante_indietro.clicked.connect(self.indietro)
-        self.layout_orizzontale.addSpacerItem(QSpacerItem(0, 80, QSizePolicy.Fixed, QSizePolicy.Fixed))
-        self.layout_orizzontale.addWidget(pulsante_indietro)
+        layout_pulsanti.addSpacerItem(QSpacerItem(50, 0, QSizePolicy.Fixed, QSizePolicy.Fixed))
+        layout_pulsanti.addWidget(pulsante_indietro)
+        layout_pulsanti.addSpacerItem(QSpacerItem(50, 0, QSizePolicy.Fixed, QSizePolicy.Fixed))
+        self.layout_verticale2.addLayout(layout_pulsanti)
