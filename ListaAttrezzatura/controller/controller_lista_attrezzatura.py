aa4c503684b96ea5168de0c662099c6f656a867a
Edoardo Tarulli <S1094923@studenti.univpm.it>
2021-05-12 12:51:05 +0200

    foto legno

--- ListaAttrezzatura/controller/controller_lista_attrezzatura.py
+++ ListaAttrezzatura/controller/controller_lista_attrezzatura.py
@@ -1,8 +1,8 @@
-from ListaAttrezzatura.model.ListaAttrezzatura import listaAttrezzatura
+from ListaAttrezzatura.model.ListaAttrezzatura import lista_attrezzatura
 
 class controller_lista_attrezzatura:
     def __init__(self):
-        self.model = listaAttrezzatura()
+        self.model = lista_attrezzatura()
 
     def aggiungi_attrezzatura(self, attrezzatura):
         self.model.aggiungi_attrezzatura(attrezzatura)
@@ -15,3 +15,6 @@ class controller_lista_attrezzatura:
 
     def prenota_attrezzatura(self, tipo):
         self.model.prenota_attrezzatura(tipo)
+
+    def get_lista_attrezzatura(self):
+        return self.model.get_lista_attrezzatura()
