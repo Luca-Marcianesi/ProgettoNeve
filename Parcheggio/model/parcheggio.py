4f5a4e92cb84394fdf2e8d6201a1d77c074e520f
Edoardo Tarulli <S1094923@studenti.univpm.it>
2021-05-17 18:00:40 +0200

    parcheggio

--- Parcheggio/model/parcheggio.py
+++ Parcheggio/model/parcheggio.py
@@ -1,3 +1,16 @@
 class parcheggio:
-    def __init__(self):
-        self.codice = 6
+    def __init__(self, codice, numero, stato, scadenza):
+        self.codice = codice
+        self.numero = numero
+        self.stato = stato
+        self.scadenza = scadenza
+
+    def get_codice(self):
+        return self.codice
+
+    def set_stato(self, stato):
+        self.stato = stato
+
+    def get_numero(self):
+        return self.numero
+
