f15a13fddaf2740dcfd0cd481bc894b7b27eef9d
Edoardo Tarulli <S1094923@studenti.univpm.it>
2021-05-19 13:08:56 +0200

    nuove viste e cose

--- ElencoDipendenti/model/elenco_dipendenti.py
+++ ElencoDipendenti/model/elenco_dipendenti.py
@@ -1,7 +1,15 @@
+import json
+import os
+import pickle
+
+from Dipendenti.model.dipendente import dipendente
+
+
 class elenco_dipendenti:
 
     def __init__(self):
         self.elenco = []
+        self.leggi_dati()
 
     def aggiungi_dipendente(self, dipendente):
         self.elenco.append(dipendente)
@@ -23,3 +31,19 @@ class elenco_dipendenti:
 
     def get_lista_elenco_dipendenti(self):
         return self.elenco
+
+
+    def salva_dati(self):
+        with open('ElencoDipendenti/data/lista_dipendenti_salvata.pickle', 'wb') as dati:
+            pickle.dump(self.elenco, dati, pickle.HIGHEST_PROTOCOL)
+
+    def leggi_dati(self):
+        if os.path.isfile('ElencoDipendenti/data/lista_dipendenti_salvata.pickle'):
+            with open('ElencoDipendenti/data/lista_dipendenti_salvata.pickle', "rb") as file:
+                self.elenco = pickle.load(file)
+        else:
+            with open("ElencoDipendenti/data/lista_dipendenti.json") as file:
+                lista_dipendenti = json.load(file)
+            for dipendenti_da_caricare in lista_dipendenti:
+                self.aggiungi_dipendente(dipendente(dipendenti_da_caricare["nome"], dipendenti_da_caricare["cognome"],
+                                 dipendenti_da_caricare["numero di telefono"]))
