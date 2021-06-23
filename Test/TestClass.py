405293d8ee08027f07708e12777448accccd08ae
Diego Mignani <S1093616@studenti.univpm.com>
2021-06-23 17:58:20 +0200

    test

--- Test/TestClass.py
+++ Test/TestClass.py
@@ -1,13 +1,19 @@
 import unittest
-from GestioneParcheggi.model.gestioneparcheggi import GestioneParcheggi
+
+from Attrezzatura.model.attrezzatura import Attrezzatura
+from GestioneParcheggi.controller.controllergestioneparcheggi import ControllerGestioneParcheggi
+from GestioneSkipass.controller.controllergestioneskipass import ControllerGestioneSkipass
 from ListaAccount.model.listaaccount import ListaAccount
+from ListaAttrezzatura.controller.controllerlistaattrezzatura import ControllerListaAttrezzatura
 from Sessione.model.sessione import Sessione
 
 
 class MyTest(unittest.TestCase):
 
     def setUp(self):
-        self.parcheggi = GestioneParcheggi()
+        self.attrezzatura = ControllerListaAttrezzatura()
+        self.parcheggi = ControllerGestioneParcheggi()
+        self.skipass = ControllerGestioneSkipass()
         self.lista = ListaAccount()
         self.lista.crea_account("Diego", "Mignani", "dieg10", "password", "20", "180", "45")
 
@@ -28,10 +34,32 @@ class MyTest(unittest.TestCase):
         assert (Sessione.get_nome() == "Diego")
         assert (Sessione.get_cognome() == "Mignani")
         assert (Sessione.get_altezza() == "180")
-        assert (Sessione.get_eta() == "22")
+        self.assertFalse(Sessione.get_eta() == "22")
 
+    # Test prenotazione parcheggio
     def test4(self):
-        self.parcheggi.prenota_parcheggio(2)
+        # Prima prenotazione
+        self.assertEqual(self.parcheggi.prenota_parcheggio(2), "Prenotazione effettuata")
+        # Doppia prenotazione
+        self.assertEqual(self.parcheggi.prenota_parcheggio(2), "Hai già una prenotazione")
+
+    # Test prenotazione skipass
+    def test5(self):
+        # Skipass settimanale
+        settimanale = self.skipass.visualizza_lista(3)
+        # Prima prenotazione
+        self.assertTrue(self.skipass.prenota_skipass(settimanale))
+        # Doppia prenotazione
+        self.assertFalse(self.skipass.prenota_skipass(settimanale))
+
+    # Test aggiunta attrezzatura da parte del proprietario e prenotazione di quell'attrezzatura dal cliente
+    def test6(self):
+        # Proprietario
+        racchette = Attrezzatura(5, "Racchette", 130)
+        self.attrezzatura.aggiungi_attrezzatura(racchette)
+        # Cliente
+        self.assertEqual(self.attrezzatura.prenota_attrezzatura(racchette), "Prenotazione effettuata")
+
 
 if __name__ == 'main':
     unittest.main()
