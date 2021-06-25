06ea4bd2f0670025b924be0bb5e51dc8f7d70d60
Diego Mignani <S1093616@studenti.univpm.com>
2021-06-25 15:32:45 +0200

    controllerskipass

--- GestioneSkipass/controller/controllergestioneskipass.py
+++ GestioneSkipass/controller/controllergestioneskipass.py
@@ -12,19 +12,13 @@ class ControllerGestioneSkipass:
     def prenota_skipass(self, skipass_selezionato):
         if Sessione.controlla_prenotazione_effettuata(self.model.codice_skipass):
             prenotazione = self.model.prenota(skipass_selezionato)
-            if prenotazione != False:
+            if prenotazione is not False:
                 Sessione.aggiungi_prenotazione(prenotazione)
-                return True
-            return False
-
-    # Metodo che chiama la funzione visualizza_lista del model
-    def get_lista_skipass(self):
-        return self.model.visualizza_lista()
+                return 'Skipass prenotato!'
+            return 'Skipass al momento non disponibile torna domani'
+        return 'Hai già prenotato uno skipass!'
 
     # Metodo che chiama la funzione get_skipass_n del model
-    def visualizza_lista(self, numero):
-        return self.model.get_skipass_n(numero)
+    def get_skipass_per_numero(self, numero):
+        return self.model.get_skipass_per_numero(numero)
 
-    # Metodo che chiama la funzione aggiungi_skipass del model
-    def aggiungi_skipass(self, skipass):
-        self.model.aggiungi_skipass(skipass)
