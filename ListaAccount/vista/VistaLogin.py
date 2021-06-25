c55e4264a4300162c2524deff2a456a6e8dd6b07
Diego Mignani <S1093616@studenti.univpm.com>
2021-06-25 15:45:52 +0200

    commenti

--- ListaAccount/vista/VistaLogin.py
+++ ListaAccount/vista/VistaLogin.py
@@ -15,32 +15,39 @@ class VistaLogin(QWidget):
     def __init__(self, parent=None):
         super(VistaLogin, self).__init__(parent)
 
-        # Attributi
-        self.accesso_view = VistaHome(self.show)
-        self.accesso_proprietario = vista_home_proprietario(self.show)
+        # Controller relativo alla vista
         self.controller = ControllerListaAccount()
-        self.crea_view = VistaCreaAccount(self.show, self.controller)
+
+        # Dizionario utilizzato per gestire i dati inseriti
         self.credenziali = {}
+
+        # Viste successive
+        self.accesso_proprietario = vista_home_proprietario(self.show)
+        self.crea_view = VistaCreaAccount(self.show, self.controller)
+        self.accesso_view = VistaHome(self.show)
+
+        # Layout usati per allineare i widget della vista
         self.layout_verticale1 = QVBoxLayout()
         self.layout_verticale2 = QVBoxLayout()
         self.layout_orizzontale1 = QHBoxLayout()
         self.layout_orizzontale2 = QHBoxLayout()
 
-        # Sfondo
+        # Funzione standard per l'inserimendo dello sfondo e del titolo nella vista
         self.show_background("SARNANONEVE")
 
         # Spaziatura tra lato sinitro e caselle
-        self.layout_orizzontale1.addSpacerItem(QSpacerItem(200, 0, QSizePolicy.Fixed, QSizePolicy.Fixed))
+        self.layout_orizzontale1.addSpacerItem(QSpacerItem(200, 0))
 
         # Caselle username e password
         self.casella_testo("USERNAME")
         casella_ps = self.casella_testo("PASSWORD")
+        # Istruzione per nascondere la digitura della password
         casella_ps.setEchoMode(QLineEdit.Password)
 
         # Spaziatura tra caselle e pulsanti
-        self.layout_verticale2.addSpacerItem(QSpacerItem(0, 50, QSizePolicy.Fixed, QSizePolicy.Fixed))
+        self.layout_verticale2.addSpacerItem(QSpacerItem(0, 50))
 
-        # Accedi
+        # Allineamento e configurazione del pulsante accedi
         accedi = QPushButton("ACCEDI")
         accedi.setFixedSize(150, 70)
         accedi.setFont(QFont('Times New Roman', 14, 100, True))
@@ -48,7 +55,7 @@ class VistaLogin(QWidget):
         self.layout_orizzontale2.addWidget(accedi)
         accedi.clicked.connect(self.call_login)
 
-        # Crea Account
+        # Allineamento e configurazione del pulsante
         crea_account = QPushButton("CREA \n ACCOUNT")
         crea_account.setFixedSize(150, 70)
         crea_account.setFont(QFont('Times New Roman', 14, 100, True))
@@ -59,7 +66,7 @@ class VistaLogin(QWidget):
 
         # Allineamento layout orizzontale1
         self.layout_orizzontale1.addLayout(self.layout_verticale2)
-        self.layout_orizzontale1.addSpacerItem(QSpacerItem(200, 0, QSizePolicy.Fixed, QSizePolicy.Fixed))
+        self.layout_orizzontale1.addSpacerItem(QSpacerItem(200, 0))
         self.layout_verticale1.addLayout(self.layout_orizzontale1)
 
         # Settaggio layout toale
@@ -71,7 +78,7 @@ class VistaLogin(QWidget):
         self.crea_view.show()
         self.close()
 
-    # Metodo per effettuare il login
+    # Metodo per effettuare il login con la differenziazione tra cliente e proprietario
     def call_login(self):
         username = self.credenziali["USERNAME"].text()
         password = self.credenziali["PASSWORD"].text()
@@ -86,9 +93,9 @@ class VistaLogin(QWidget):
             QMessageBox.critical(self, 'Errore', 'Credenziali errate',
                                  QMessageBox.Ok, QMessageBox.Ok)
 
-    # Creazione, settaggio e stile per una casella di testo
+    # Metodo standard utile per la creazione della casella di testo
     def casella_testo(self, tipo):
-        # Label
+        # Label sopra la casella di testo
         label = QLabel(tipo + ":")
         label.setFont(QFont('Times New Roman', 25))
         label.setAlignment(Qt.AlignCenter)
@@ -105,7 +112,7 @@ class VistaLogin(QWidget):
 
     # Creazione, settaggio e stile dello sfondo
     def show_background(self, stringa):
-        # Sfondo
+        # Sfondo della vista
         self.setFixedWidth(800)
         self.setFixedHeight(600)
         back_img = QImage("Data/Immagini/VistaLogin.png")
@@ -114,7 +121,7 @@ class VistaLogin(QWidget):
         palette.setBrush(10, QBrush(img))
         self.setPalette(palette)
 
-        # Titolo
+        # Titolo della finestra
         titolo = QLabel(stringa)
         titolo.setAlignment(Qt.AlignCenter)
         titolo.setFont(QFont('Times New Roman', 40))
